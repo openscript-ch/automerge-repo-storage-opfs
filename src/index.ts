e02b40debbb575ee6d7f4b0352dc79d20f21de51
Robin Bühler <git+r@obin.ch>
2024-01-11 10:21:38 +0000

    chore: scaffold project structure

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { Chunk, StorageAdapter, StorageKey } from '@automerge/automerge-repo';
+
+export class OPFSStorageAdapter extends StorageAdapter {
+  load(key: StorageKey): Promise<Uint8Array | undefined> {
+    throw new Error('Method not implemented.');
+  }
+  save(key: StorageKey, data: Uint8Array): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  remove(key: StorageKey): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  loadRange(keyPrefix: StorageKey): Promise<Chunk[]> {
+    throw new Error('Method not implemented.');
+  }
+  removeRange(keyPrefix: StorageKey): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
